85c5e4805e734d47cf82b77fa2223d614bd03200
FabioSpr <fabio.spreafico@txtgroup.com>
2025-10-01 17:41:44 +0200

    extension build and vsix file deploy

---
+++ out/extention.js
@@ -0,0 +1,34 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.deactivate = exports.activate = void 0;
+const vscode = require("vscode");
+function activate(context) {
+    console.log("Log Outline Extension activated");
+    context.subscriptions.push(vscode.languages.registerDocumentSymbolProvider({ language: 'log', scheme: 'file' }, new LogTestSymbolProvider()));
+}
+exports.activate = activate;
+function deactivate() { }
+exports.deactivate = deactivate;
+class LogTestSymbolProvider {
+    provideDocumentSymbols(document) {
+        const symbols = [];
+        const testRegex = /^\s*\d{2}:\d{2}:\d{2}\.\d{3}\s*---\s*STARTING TEST\s+(.+?)(?:\s*---|$)/;
+        const sequenceRegex = /^\s*\d{2}:\d{2}:\d{2}\.\d{3}\s*---\s*TEST SEQUENCE\s+(.+?)\s*---$/;
+        for (let i = 0; i < document.lineCount; i++) {
+            const line = document.lineAt(i);
+            let match = testRegex.exec(line.text);
+            if (match) {
+                const testName = match[1].trim();
+                symbols.push(new vscode.DocumentSymbol(testName, '', vscode.SymbolKind.Namespace, line.range, line.range));
+                continue;
+            }
+            match = sequenceRegex.exec(line.text);
+            if (match) {
+                const seqName = match[1].trim();
+                symbols.push(new vscode.DocumentSymbol(seqName, '', vscode.SymbolKind.Namespace, line.range, line.range));
+            }
+        }
+        return symbols;
+    }
+}
+//# sourceMappingURL=extention.js.map
